e17037e390b4517a0bade2c6f35868e6d195ee5c
Luciano Paci <lucianopaci93@gmail.com>
2020-12-13 13:01:56 -0300

    First Commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: 'always',
+    useTabs: false,
+    tabWidth: 2,
+    printWidth: 80
+  }
+  
